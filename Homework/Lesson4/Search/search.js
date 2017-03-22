55efc14b37e7fd4710509cdec67d5f4d08ea7589
svenchamby <svenchamby@gmail.com>
2017-03-22 11:18:04 -0600

    Search Activity Progress

---
+++ Homework/Lesson4/Search/search.js
@@ -0,0 +1,8 @@
+$('#query').keyup(function () {
+	//all code will be inside of this block
+	var value = $('#query').val();
+	var rExp = new RegExp(value, "i");
+	$.getJSON("//autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
+			console.log(data);
+		}) // end getJSON
+}); // end keyup
