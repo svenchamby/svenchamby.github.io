f4f6889db42827484135c610c4be24ab6e6a0618
svenchamby <svenchamby@gmail.com>
2017-03-22 11:44:31 -0600

    Search Tool Activity

--- Homework/Lesson4/Search/search.js
+++ Homework/Lesson4/Search/search.js
@@ -4,5 +4,15 @@ $('#query').keyup(function () {
 	var rExp = new RegExp(value, "i");
 	$.getJSON("//autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function (data) {
 			console.log(data);
+			var output = '<ol>';
+			$.each(data.RESULTS, function (key, val) {
+					if (val.name.search(rExp) != -1) {
+						output += '<li>';
+						output += '<a href="//www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+						output += '</li>';
+					}
+				}) // end each
+			output += '</ol>';
+			$("#searchResults").html(output); // send results to the page
 		}) // end getJSON
 }); // end keyup
